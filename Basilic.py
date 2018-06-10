af278d480fa494cbab1bcc9830bbc0a63995201b
Gui <33119610+GuiMarion@users.noreply.github.com>
2018-06-10 21:29:24 +0200

    Final

--- Basilic.py
+++ Basilic.py
@@ -1,26 +1,25 @@
-
 import datetime
 import Plante
 
-class Basilic(Plante.Plante):
 
-	# TODO afficher arrosage et ajustement taille lampe
+class Basilic(Plante.Plante):
 
 	def __init__(self, name):
 		Plante.Plante.__init__(self,name)
 		self.type = "basilic"
+		self.state = 3
 		self.etapes = [
-			"A1",
-			"A2",
-			"A3",
-			"A4",
-			"A5",
-			"A6",
-			"A7",
-			"A8",
-			"A9",
-			"A10",
-			"A11"
+			"A1", # Preparation
+			"A2", # Germination
+			"A3", # Fin Germination 
+			"A4", # Mise en terre
+			"A5", # Lampes
+			"A6", # Engrais
+			"A7", # Installation lampe
+			"A8", # Croissance
+			"A9", # Rampotage/ Croissance
+			"A10", # Floraison
+			"A11" # Fin
 		]
 
 
@@ -32,7 +31,6 @@ class Basilic(Plante.Plante):
 
 			return(self)
 
-
 	def A5(self, interface, reponse=None):
 		# Acquisition lumière
 		if self.state == 4 :
@@ -42,15 +40,16 @@ class Basilic(Plante.Plante):
 			interface.acquisition("Votre plante est desormais en terre, il lui faudra maintenant de la lumière pour vivre.\
 				Vous pouvez utiliser une lampe agricole ou bien la lumière naturelle, cependant \
 				il vous faudra beaucoup de lumière, la lumière naturelle sera donc appropriée dans une région\
-				ensoleillée et qui n'est pas à l'ombre. Une lampe agricole peut être utilisée seule ou en\
-				complément et devra être d'au moins 125-250 watts (ce qui a un coût en élécricité.)\n\
+				ensoleillée à un endroit qui n'est pas à l'ombre. Une lampe agricole peut être utilisée seule ou en\
+				complément et devra être d'au moins 125-250 watts (ce qui a un coût non négligeable en élécricité.)\n\
 				Que voulez-vous faire ?",["lampe", "lumiere naturelle"], [[self, "A5", [interface, "lampe"]], [self, "A5", [interface, "lumiere naturelle"]]])
 
 
 		elif reponse == "lampe":
 			self.Lampe = True
 			interface.acquisition("C'est un très bon choix. La lampe est-elle une \
-				<def = Les lampes HPS émettent une lumière orange et leur spectre de couleurs est mieux adapté pour la floraison ou la fructification.>HPS</def> ? "
+			<def = Lampe à décharge qui est très puissante mais consomme beacoup d'éléctricité et chauffe énorméménent. Si vous optez pour cette \
+			solution il faudrait être vigilant avec la temperature.>HPS</def> ? "
 			,["Oui", "Non"], [[self, "A5", [interface, "HPS"]], [self, "A5", [interface, "non HPS"]]])
 
 
@@ -58,7 +57,7 @@ class Basilic(Plante.Plante):
 			self.Lampe = False
 			interface.acquisition("C'est un très bon choix. Nous attirons votre attention sur le fait qu'il vous faut\
 				être attentif à l'ensoleillement de votre plante (au moins 6h de plein soleil par jours)\
-				ainsi qu'au risque climatique et naturels (grêle, pluit, vent, ...) que notre application\
+				ainsi qu'aux risques climatiques et naturels (grêle, pluit, vent, ...) que notre application\
 				ne pourra pas prévoir.",["ok"], [[self, "A6", [interface, None]]])
 
 		else :
@@ -76,32 +75,40 @@ class Basilic(Plante.Plante):
 			self.A6(interface, None)
 
 
-
-
-
 	def A7(self, interface):
 		# Affichage croissance
-		if self.state < 7 :
+		if self.state == 6 :
 			self.state = 7
 
 		if self.Lampe:
-				interface.acquisition("Vous allez devoir installer votre lampe agricole. Il faut la placer sur un porte lampe, que vous pouvez\
-					construire vous même avec des cables/ficelle et une structure métalique, il faut que l'installation permette\
-					de changer la hauteur de la lampe quand on le désire. Placer pour l'instant la lampe 10 cm au dessus de la plante.\
-					La <def = La photopériode est le rapport entre la durée du jour et la durée de la nuit>photopériode</def> \
-					doit être reglée à 18h de lumière par jour, utiliser un minuteur que vous pourrez\
-					vous procurer dans le commerce. Utiliser un reflecteur au dessus de la lampe afin de perdre le moins\
-					de lumière possible est une bonne pratique.\n\
-					Une fois fait, bravo ! Votre plante est desormais en phase de croissance, vous n'avez plus qu'à utiliser\
-					l'application pour savoir quand arroser et mettre les engrais, nous vous demanderons régulièrement\
+			interface.acquisition("Vous allez devoir installer votre lampe agricole. Il faut la placer sur un porte lampe, que vous pouvez\
+				construire vous même avec des cables/ficelle et une structure métalique, il faut que l'installation permette\
+				de changer la hauteur de la lampe quand on le désire. Placer pour l'instant la lampe 10 cm au dessus de la plante.\
+				La photopériode doit être reglée à 18h de lumière par jour, utiliser un minuteur que vous pourrez\
+				vous procurer dans le commerce. Utiliser un reflecteur au dessus de la lampe afin de perdre le moins\
+				de lumière possible est une bonne pratique.", ["ok"], [[interface, "accueil"]])
+		else :
+			self.A8(interface)
+
+
+	def A8(self, interface):
+		# Affichage croissance
+		if self.state == 7 :
+			self.state = 8
+
+		if self.Lampe:
+
+				interface.acquisition("Votre plante est desormais en phase de croissance, vous n'avez plus qu'à utiliser \
+					l'application pour savoir quand arroser et mettre les engrais, nous vous demanderons régulièrement \
 					la taille de vos plantes afin de vous indiquer les prochaines démarches."
 					,["ok"], [[interface, "accueil"]])
 
 		else :
-				interface.acquisition("Bravo ! Votre plante est desormais en phase de croissance, vous n'avez plus qu'à utiliser\
-					l'application pour savoir quand arroser et mettre les engrais, nous vous demanderons régulièrement\
+				interface.acquisition("Bravo ! Votre plante est desormais en phase de croissance, vous n'avez plus qu'à utiliser \
+					l'application pour savoir quand arroser et mettre les engrais, nous vous demanderons régulièrement \
 					la taille de vos plantes afin de vous indiquer les autres démarches à suivre."
 					, ["ok"], [[interface, "accueil"]])
+
 
 
 
@@ -113,21 +120,21 @@ class Basilic(Plante.Plante):
 		self.acquisition_taille(interface)
 
 		if (self.Taille >15 and self.Pot and self.Rempoter == 0) or (self.Taille > 25 and self.Pot and self.Rempoter == 1):
-			interface.acquisition("Il est temps de <def = rempoter est l'acte de changer une plante de pot>rempoter</def> votre plante.\
-				Pour cela attendez le prochain arrosage et avant d'arroser,\
-				sortez votre plante de son pot en tapant sur l'arrière du pot. Vous devriez pouvoir sortir la motte de\
+			interface.acquisition("Il est temps de <def = Déplacer la plante dans un pot plus grand>rempoter</def> votre plante.\
+			 	Pour cela attendez le prochain arrosage et avant d'arroser,\
+				sortez votre plante de son pot en tapant sur l'arrière. Vous devriez pouvoir sortir la motte de\
 				terre sans l'abimer. Ensuite remplissez aux trois quarts le pot de taille supèrieure, creuser un trou\
-				de la taille de la motte de terre que vous avez sortie, et glissez là à l'interieur. Soupoudrez le tout\
+				de la taille de la motte de terre que vous avez sortie, et glissez la à l'interieur. Soupoudrez le tout\
 				de terreau, tassez, puis arrosez."
-				, ["d'accord"], [[interface, "accueil"]])
+				, ["ok"], [[interface, "accueil"]])
 
 			self.Rempoter +=1
 
 		elif self.Taille > 30 and self.Lampe:
 			interface.acquisition("Il est temps de passer à la période de floraison, pour cela il faut changer de lampe, pour\
 				mettre une lampe spéciale floraison (elle a un spectre plus chaud, plus jaune). Et réduire la\
-				<def = La photopériode est le rapport entre la durée du jour et la durée de la nuit>photopériode</def> à 12h de lumière par jour."
-				, ["d'accord"], [[interface, "accueil"]])
+				<def = La photopériode est le rapport entre la durée d'ensoleimment sur une journée>photopériode</def> à 12h de lumière par jour."
+				, ["ok"], [[interface, "accueil"]])
 
 		elif self.Taille > 35:
 			interface.acquisition("Vous devriez voir apparaître les premières fleurs, si c'est le cas, il est temps d'effectuer\
@@ -135,7 +142,7 @@ class Basilic(Plante.Plante):
 			la lumière (les gourmandes) et les parties qui ne prendrons pas la lumière. Ne coupez pas les grosses feuilles,\
 			elles contiennent beaucoup d'energie que la plante peut quand même utiliser, s'il elle n'en a plue besoin\
 			elle s'en débarassera d'elle même."
-				, ["d'accord"], [[self, "A10", [interface, None]]])
+				, ["ok"], [[self, "A10", [interface, None]]])
 
 			self.A10(interface)
 
@@ -150,21 +157,19 @@ class Basilic(Plante.Plante):
 				, ["ok"], [[interface, "accueil"]])
 
 
-
-
 	def A10(self, interface, reponse=None):
 		# Acquisition floraison
-		if self.state < 10 :
+		if self.state == 9 :
 			self.state = 10
 
 		if reponse == None :
 			interface.acquisition("Vous devriez être passé en floraison et avoir effectué les premières tailles. Est-ce bien le cas ?"
-				, ["oui", "non"], [[self, "A10", [interface, "oui"]], [self, "A10", [interface, "non"]]])
+				, ["Oui", "Non"], [[self, "A10", [interface, "oui"]], [self, "A10", [interface, "non"]]])
 
 		elif reponse == 'oui' :
 			self.Floraison = True
 			self.Floraison_début = datetime.date.today()
-			interface.acquisition("Très bien vous pouvons continuer"
+			interface.acquisition("Très bien nous pouvons continuer"
 				, ["ok"], [[self, "A11", interface]])
 
 		elif reponse == "non" :
